2366d10f8f960cd8be6cd90f0f92d882a8bd7aec
周优优 <moloach@163.com>
2020-10-22 09:03:37 +0800

    update init script

--- init.sh
+++ init.sh
@@ -2,21 +2,29 @@
 
 #reference:https://www.yuque.com/sunhl/mvumc2/rq64vb
 
+cat >> /etc/pacman.conf << EOF
+[archlinuxcn]
+SigLevel=Never
+Server = https://mirrors.ustc.edu.cn/archlinuxcn/$arch
+EOF
+
+#feh --randomize --bg-fill ~/Pictures/DesktopBackground
+
+# xorg et.
+pacman -S xorg-server xorg-xinit firefox  git 
+
 # fonts 
-pacman -S wqy-microhei ttf-font-awesome ttf-liberation
-yay -S siji-git
+pacman -S wqy-microhei ttf-font-awesome ttf-liberation wqy-zenhei noto-fonts noto-fonts-cjk noto-fonts-emoji
+yay -S siji-git -y
+
+# 系统调节(pactl/音量调节,alsa-utils/音量调节)
+yay -S alsa-utils light pactl NetworkManager
 
 # i3 and some useful softwares
-pacman -S i3-gaps polybar alacritty picom feh rofi nautilus  volumeicon xflux xorg-xinit git -y
+pacman -S i3-gaps polybar alacritty picom feh rofi nautilus volumeicon xflux  ranger
 
-# 输入法
-pacman -S fcitx-im fcitx-configtool
-
-pacman -S fcitx
-
-
-# 系统调节
-yay -S alsa-utils light
+# 输入法 #fcitx-googlepinyin
+pacman -S fcitx-im fcitx-configtool fcitx-sougoupinyin 
 
 cat >> ~/.bashrc << EOF
 export LANG=zh_CN.UTF-8
