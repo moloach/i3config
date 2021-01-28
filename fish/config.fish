74fc7430c19e14301cf8a33598534fa1d46c69e8
moloach <moloach@163.com>
2021-01-28 09:28:33 +0800

    add pyenv as default

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,9 @@
 source $HOME/.cargo/env
 set PATH /home/moloach/.dotnet $PATH
 set PATH /home/moloach/.local/bin $PATH
+
+
+# pyenv init
+if command -v pyenv 1>/dev/null 2>&1
+  pyenv init - | source
+end
